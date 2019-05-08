dac1c0c52477224d139fcc08b4a039a0380179d3
Grabiel De La Nieve <grabieldelanieve@gmail.com>
2019-05-08 16:46:03 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://grabiel-snow:Delanieve7@cluster0-hm8ei.mongodb.net/test?retryWrites=true';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
